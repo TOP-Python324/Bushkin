96a893c30906ea9879743383d0bd96913bc6c4d1
Vladislav732010 <volgograd732010@yandex.ru>
2023-06-05 18:47:04 +0300

    'доработал'

--- 2023.05.28/1.py
+++ 2023.05.28/1.py
@@ -1,15 +1,14 @@
 sum = 0
 print('Введите последовательно три числа:')
-digit1 = float(input())
-digit2 = float(input())
-digit3 = float(input())
-if digit1 > 0 :
-    sum += digit1
-if digit2 > 0 :
-    sum += digit2
-if digit3 > 0 :
-    sum += digit3
+i = 0
+while i < 3 :
+    digit = float(input())
+    i += 1
+    if digit > 0:
+        sum += digit
 print(f'Сумма положительных чисел:\n{sum}')
+
+
 
 # Введите последовательно три числа:
 # 13
