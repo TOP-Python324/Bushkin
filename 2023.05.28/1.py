54f5b198f03ffce26e4a1a3946e9258b6782a5b8
Vladislav732010 <volgograd732010@yandex.ru>
2023-06-07 10:16:32 +0300

    'запутался'

--- 2023.05.28/1.py
+++ 2023.05.28/1.py
@@ -1,14 +1,15 @@
 sum = 0
 print('Введите последовательно три числа:')
-i = 0
-while i < 3 :
-    digit = float(input())
-    i += 1
-    if digit > 0:
-        sum += digit
+digit1 = float(input())
+digit2 = float(input())
+digit3 = float(input())
+if digit1 > 0 :
+    sum += digit1
+if digit2 > 0 :
+    sum += digit2
+if digit3 > 0 :
+    sum += digit3
 print(f'Сумма положительных чисел:\n{sum}')
-
-
 
 # Введите последовательно три числа:
 # 13
